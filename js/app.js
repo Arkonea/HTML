5cdac42a35ff4fda10e69dbe5cbd965e25286879
Ivan vivi <ivanvivivela@gmail.com>
2025-05-05 12:23:49 -0500

    inicio de actividad

---
+++ js/app.js
@@ -0,0 +1,14 @@
+lef numero1 = parseInt(prompt ("Ingerese numero "))
+lef numero2 = parseInt(prompt ("Ingerese numero "))
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multiplicacion = numero1 * numero2
+let division = numero1 / numero2
+
+
+ document.writeln ("La suma es: " + suma + "<br>")
+ document.writeln ("La resta es: " + resta + "<br>")
+ document.writeln ("La multiplicacion es: " + multiplicacion + "<br>")
+ document.writeln ("La division es: " + division + "<br>")  
+
+
